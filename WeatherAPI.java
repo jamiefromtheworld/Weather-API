ee10649543bcd55a280a62e8307501ec5c616850
Jamie <108243497+jamiefromtheworld@users.noreply.github.com>
2023-11-15 21:28:13 -0800

    w

--- WeatherAPI.java
+++ WeatherAPI.java
@@ -4,6 +4,10 @@ import java.io.*;
 public class WeatherAPI {
    public static void main(String[] args) throws FileNotFoundException {
       Scanner lineScan = new Scanner(new File("WeatherAPI.txt"));
+      getInfo(lineScan);
+      
+   }
+   public static void  getInfo(Scanner in) {
       
    }
 }
