d0eadcbd2786173c630df648a0c102fa991ced7e
Jamie <108243497+jamiefromtheworld@users.noreply.github.com>
2023-11-15 22:59:20 -0800

    h

--- start.py
+++ start.py
@@ -1,1 +1,47 @@
-import requests
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 47.755653,
+	"longitude": -122.341515,
+	"daily": "precipitation_sum",
+	"precipitation_unit": "inch",
+	"timezone": "America/Los_Angeles",
+	"past_days": 3,
+	"forecast_days": 1
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_precipitation_sum = daily.Variables(0).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s"),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+daily_data["precipitation_sum"] = daily_precipitation_sum
+
+daily_dataframe = pd.DataFrame(data = daily_data)
+print(daily_dataframe)
+
